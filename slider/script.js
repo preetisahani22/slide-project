fef21374fc2a4227de97268dbf8356788a8c9b70
preetisahani22 <preetisahani7417@gmail.com>
2026-04-14 16:46:50 +0530

    Add files via upload

---
+++ slider/script.js
@@ -0,0 +1,32 @@
+const sliders = [
+    {
+        img : "https://wallpapercave.com/wp/wp4990451.jpg"
+    },
+    {
+        img : "https://cdn.wallpapersafari.com/84/81/ZGTg0z.jpg"
+    },
+    {
+        img : "https://cdn.wallpapersafari.com/55/15/Ek2XQl.jpg"
+    }
+];
+
+const slidersContainer = document.querySelector('#sliders img');
+
+let currentQuote = 0;
+
+const nextQuote = ()=> {
+    slidersContainer.src = sliders[currentQuote].img;
+    
+    currentQuote++;
+    if(currentQuote > sliders.length - 1){
+        currentQuote = 0
+    }
+}
+
+nextQuote();
+
+const prevQuote = ()=> {
+    slidersContainer.src = sliders[currentQuote].img;
+
+    currentQuote--;
+}
